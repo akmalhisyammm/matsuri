4f2f8650e0937a3d93537b839912ae6727b1be7b
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-10-28 20:46:01 +0700

    feat(api): setup users controller, model, and router

---
+++ app/api/v1/users/model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      minLength: 3,
+      maxLength: 50,
+      required: [true, 'Name is required.'],
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: [true, 'Email is required.'],
+    },
+    password: {
+      type: String,
+      minLength: 6,
+      required: [true, 'Password is required.'],
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'organizer', 'owner'],
+      default: 'admin',
+    },
+    organizerId: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Organizer',
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 12);
+  }
+
+  next();
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  const isMatch = await bcrypt.compare(password, this.password);
+
+  return isMatch;
+};
+
+module.exports = mongoose.model('User', userSchema);
