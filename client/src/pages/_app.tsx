aa035af6ea74928984bc39bfc12b60303277deba
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-03 00:05:39 +0700

    chore(pages): setup app and document file

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,8 +1,14 @@
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 
-import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/main.css';
 
 const App = ({ Component, pageProps }: AppProps) => {
+  useEffect(() => {
+    require('bootstrap/dist/js/bootstrap.bundle.min');
+  }, []);
+
   return <Component {...pageProps} />;
 };
 
