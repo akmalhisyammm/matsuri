c05961efae93fcf3e64d42e7f43e540cb7475d9a
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-06 18:23:48 +0700

    chore(pages): integrate auth context to app

--- client/src/pages/_app.tsx
+++ client/src/pages/_app.tsx
@@ -1,15 +1,26 @@
 import { useEffect } from 'react';
-import type { AppProps } from 'next/app';
+import { ToastContainer } from 'react-toastify';
+
+import { AuthProvider } from 'contexts/auth';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-toastify/dist/ReactToastify.css';
+import 'react-loading-skeleton/dist/skeleton.css';
 import '../styles/main.css';
+
+import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
   useEffect(() => {
     require('bootstrap/dist/js/bootstrap.bundle.min');
   }, []);
 
-  return <Component {...pageProps} />;
+  return (
+    <AuthProvider>
+      <Component {...pageProps} />
+      <ToastContainer position="top-right" />
+    </AuthProvider>
+  );
 };
 
 export default App;
