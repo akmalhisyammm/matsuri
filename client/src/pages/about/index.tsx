5df806ec6a44be62ad4c00e1c23700b33f91b2ca
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-06 23:44:51 +0700

    feat(pages): integrate not found organism to not found page

---
+++ client/src/pages/about/index.tsx
@@ -0,0 +1,12 @@
+import { NotFound } from 'components/organisms';
+import Layout from 'components/layout';
+
+const About = () => {
+  return (
+    <Layout title="About">
+      <NotFound message="Under Construction" />
+    </Layout>
+  );
+};
+
+export default About;
---
+++ client/src/pages/browse/index.tsx
@@ -0,0 +1,12 @@
+import { NotFound } from 'components/organisms';
+import Layout from 'components/layout';
+
+const Browse = () => {
+  return (
+    <Layout title="Browse">
+      <NotFound message="Under Construction" />
+    </Layout>
+  );
+};
+
+export default Browse;
