e58c549f045f5aa935c35da2bb40e42e24a48ba5
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-06 23:53:58 +0700

    feat(pages): integrate organisms to sign up and activate pages

--- client/src/components/organisms/SponsorList/index.tsx
+++ client/src/components/organisms/SponsorList/index.tsx
@@ -10,11 +10,11 @@ const SponsorList = ({ theme }: SponsorListProps) => {
       <p>Events held by top & biggest global companies</p>
 
       <div className="d-flex flex-row flex-wrap justify-content-center align-items-center">
-        <Image src="/images/apple-111.svg" alt="semina" width={88} height={30} />
-        <Image src="/images/Adobe.svg" alt="semina" width={127} height={30} />
-        <Image src="/images/slack-21.svg" alt="semina" width={119} height={30} />
-        <Image src="/images/spotify-11.svg" alt="semina" width={101} height={30} />
-        <Image src="/images/google-2015.svg" alt="semina" width={93} height={30} />
+        <Image src="/images/apple-111.svg" alt="Apple" width={88} height={30} />
+        <Image src="/images/Adobe.svg" alt="Adobe" width={127} height={30} />
+        <Image src="/images/slack-21.svg" alt="Slack" width={119} height={30} />
+        <Image src="/images/spotify-11.svg" alt="Spotify" width={101} height={30} />
+        <Image src="/images/google-2015.svg" alt="Google" width={93} height={30} />
       </div>
     </section>
   );
