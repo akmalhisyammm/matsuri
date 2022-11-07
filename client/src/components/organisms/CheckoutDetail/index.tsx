7e151caace09cd3dcce07b2225892574dfda2ed0
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-07 07:22:29 +0700

    feat(components): add checkout and order organisms

--- client/src/components/organisms/CheckoutDetail/index.tsx
+++ client/src/components/organisms/CheckoutDetail/index.tsx
@@ -1,36 +1,76 @@
 import Image from 'next/image';
+import Skeleton from 'react-loading-skeleton';
+import moment from 'moment';
 
-const CheckoutDetail = () => {
+import { useEventDetail } from 'hooks/events';
+import { formatDate } from 'utils/formatDate';
+
+import type { IEventDetailSWR } from 'types/event';
+
+type CheckoutDetailProps = {
+  eventId: string;
+  ticketId: string;
+};
+
+const CheckoutDetail = ({ eventId, ticketId }: CheckoutDetailProps) => {
+  const { data, isLoading, isError }: IEventDetailSWR = useEventDetail(eventId);
+
   return (
     <section className="event-details container d-flex flex-wrap justify-content-lg-center align-items-center gap-5">
-      <Image
-        src="/images/details-image.png"
-        className="event-image"
-        alt="Event Image"
-        width={2414}
-        height={1356}
-        priority
-      />
-      <div className="d-flex flex-column gap-3">
-        <h5>
-          Start Your Design Career <br className="d-none d-md-block" />
-          With Design Sprint
-        </h5>
+      {!isError ? (
+        <>
+          {!isLoading ? (
+            <Image
+              src={`${process.env.NEXT_PUBLIC_API_URL}/${data?.image.url}`}
+              className="event-image"
+              alt={data?.title}
+              width={2414}
+              height={1356}
+              priority
+            />
+          ) : (
+            <Skeleton width={368} height={255} borderRadius={30} />
+          )}
 
-        <div className="d-flex align-items-center gap-3">
-          <Image src="/icons/ic-marker-white.svg" alt="" width={32} height={32} />
-          <span>Gowork, Bandung</span>
-        </div>
-        <div className="d-flex align-items-center gap-3">
-          <Image src="/icons/ic-time-white.svg" alt="" width={32} height={32} />
-          <span>15.00 PM WIB</span>
-        </div>
-        <div className="d-flex align-items-center gap-3">
-          <Image src="/icons/ic-calendar-white.svg" alt="" width={32} height={32} />
-          <span>22 Agustus 2022</span>
-        </div>
-      </div>
-      <div className="total-price">$2,980</div>
+          <div className="d-flex flex-column gap-3">
+            {!isLoading ? (
+              <>
+                <h5>{data?.title}</h5>
+
+                <div className="d-flex align-items-center gap-3">
+                  <Image src="/icons/ic-marker-white.svg" alt="" width={32} height={32} />
+                  <span>{data?.venueName}</span>
+                </div>
+                <div className="d-flex align-items-center gap-3">
+                  <Image src="/icons/ic-time-white.svg" alt="" width={32} height={32} />
+                  <span>{moment(data?.date).format('HH:MM A')}</span>
+                </div>
+                <div className="d-flex align-items-center gap-3">
+                  <Image src="/icons/ic-calendar-white.svg" alt="" width={32} height={32} />
+                  <span>{formatDate(data?.date)}</span>
+                </div>
+              </>
+            ) : (
+              <Skeleton count={8} width={240} />
+            )}
+          </div>
+
+          {!isLoading ? (
+            data?.tickets.map(
+              (ticket) =>
+                ticket._id === ticketId && (
+                  <div key={ticket._id} className="total-price">
+                    {ticket.price === 0 ? 'Free' : `$${ticket.price}`}
+                  </div>
+                )
+            )
+          ) : (
+            <Skeleton width={200} height={50} />
+          )}
+        </>
+      ) : (
+        <p>Failed to fetch checkout detail.</p>
+      )}
     </section>
   );
 };
--- client/src/components/organisms/OrderList/index.tsx
+++ client/src/components/organisms/OrderList/index.tsx
@@ -1,7 +1,9 @@
 import Image from 'next/image';
+import Skeleton from 'react-loading-skeleton';
 import moment from 'moment';
 
 import { useParticipantOrders } from 'hooks/orders';
+import { formatDate } from 'utils/formatDate';
 
 import type { IOrderSWR } from 'types/order';
 
@@ -12,31 +14,39 @@ const OrderList = () => {
     <section className="bg-navy">
       <div className="checkout container">
         <div className="text-center checkout-title">Your Order List</div>
-        {data.map((order) => (
-          <div
-            className="event-details container d-flex flex-wrap justify-content-lg-center align-items-center gap-5"
-            key={order._id}>
-            <div className="d-flex flex-column gap-3">
-              <h5>{order.eventHistory.title}</h5>
+        {!isError ? (
+          !isLoading ? (
+            data?.map((order) => (
+              <div
+                className="event-details container d-flex flex-wrap justify-content-lg-center align-items-center gap-5"
+                key={order._id}>
+                <div className="d-flex flex-column gap-3">
+                  <h5>{order.eventHistory.title}</h5>
 
-              <div className="d-flex align-items-center gap-3">
-                <Image src="/icons/ic-marker-white.svg" alt="" />
-                <span>{order.eventHistory.venueName}</span>
+                  <div className="d-flex align-items-center gap-3">
+                    <Image src="/icons/ic-marker-white.svg" alt="" width={32} height={32} />
+                    <span>{order.eventHistory.venueName}</span>
+                  </div>
+                  <div className="d-flex align-items-center gap-3">
+                    <Image src="/icons/ic-time-white.svg" alt="" width={32} height={32} />
+                    <span>{moment(order.eventHistory.date).format('HH:MM A')}</span>
+                  </div>
+                  <div className="d-flex align-items-center gap-3">
+                    <Image src="/icons/ic-calendar-white.svg" alt="" width={32} height={32} />
+                    <span>{formatDate(order.eventHistory.date)}</span>
+                  </div>
+                </div>
+                <div className="total-price">
+                  {order.totalPay === 0 ? 'Free' : `$${order.totalPay}`}
+                </div>
               </div>
-              <div className="d-flex align-items-center gap-3">
-                <Image src="/icons/ic-time-white.svg" alt="" />
-                <span>{moment(order.eventHistory.date).format('HH.MM A')}</span>
-              </div>
-              <div className="d-flex align-items-center gap-3">
-                <Image src="/icons/ic-calendar-white.svg" alt="" />
-                <span>{order.eventHistory.date}</span>
-              </div>
-            </div>
-            <div className="total-price">
-              {order.totalPay === 0 ? 'Free' : `$${order.totalPay}`}
-            </div>
-          </div>
-        ))}
+            ))
+          ) : (
+            <Skeleton count={8} width={240} />
+          )
+        ) : (
+          <p className="text-center mt-5">Failed to fetch order list.</p>
+        )}
       </div>
     </section>
   );
