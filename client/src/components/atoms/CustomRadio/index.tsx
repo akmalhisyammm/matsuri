6171f3e4014e12de5679736f81dee7d28d9805a3
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-07 07:21:44 +0700

    refactor(components): adjust some components

--- client/src/components/atoms/CustomRadio/index.tsx
+++ client/src/components/atoms/CustomRadio/index.tsx
@@ -3,19 +3,20 @@ import Image from 'next/image';
 type CustomRadioProps = {
   variant: string;
   name: string;
+  id: string;
   label: string;
   imageUrl: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const CustomRadio = ({ variant, name, label, imageUrl, onChange }: CustomRadioProps) => {
+const CustomRadio = ({ variant, name, id, label, imageUrl, onChange }: CustomRadioProps) => {
   return (
     <label className={`${variant} h-100 d-flex justify-content-between align-items-center`}>
       <div className="d-flex align-items-center gap-4">
-        <Image src={imageUrl} alt={label} width={65} height={40} />
+        <Image src={imageUrl} alt={label} width={65} height={0} style={{ height: 'auto' }} />
         <div>{label}</div>
       </div>
-      <input type="radio" name={name} onChange={onChange} />
+      <input type="radio" name={name} id={id} onChange={onChange} />
       <span className="checkmark" />
     </label>
   );
