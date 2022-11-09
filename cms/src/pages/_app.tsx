da0f7bad142ea1e8a7ce676e618f4ce57b62a237
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-09 22:14:12 +0700

    chore: integrate chakra ui into cms

--- cms/src/pages/_app.tsx
+++ cms/src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import type { AppProps } from "next/app";
+import { ChakraProvider } from '@chakra-ui/react';
+
+import customTheme from 'styles/customTheme';
+
+import type { AppProps } from 'next/app';
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />;
+  return (
+    <ChakraProvider theme={customTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 };
 
 export default App;
