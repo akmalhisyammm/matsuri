a3b37ff8ff75a00e16c8de706e7a5e29184a7eed
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-10 11:03:10 +0700

    feat(contexts): setup auth context

---
+++ cms/src/contexts/auth/AuthProvider.tsx
@@ -0,0 +1,117 @@
+import { useToast } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import jwtDecode from 'jwt-decode';
+
+import AuthContext from './Auth.context';
+import { postFetcher } from 'utils/fetcher';
+import { getToken, removeToken, setToken } from 'utils/storeToken';
+
+import type { IJWTPayload, IUser } from 'types/user';
+
+type AuthProviderProps = {
+  children: React.ReactNode;
+};
+
+const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<IUser | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+  const toast = useToast();
+  const token = getToken();
+
+  const signIn = async (email: string, password: string) => {
+    setIsLoading(true);
+
+    try {
+      const { data } = await postFetcher('/auth/sign-in', { email, password });
+
+      setToken(data.token);
+      setIsAuthenticated(true);
+
+      toast({
+        title: 'Success',
+        description: 'Sign in successful.',
+        status: 'success',
+        duration: 3000,
+      });
+
+      router.push('/dashboard');
+    } catch (err) {
+      if (err instanceof Error) {
+        toast({
+          title: 'Error',
+          description: err.message,
+          status: 'error',
+          duration: 3000,
+        });
+      }
+    }
+
+    setIsLoading(false);
+  };
+
+  const signOut = () => {
+    setIsLoading(true);
+
+    removeToken();
+
+    setIsAuthenticated(false);
+    setIsLoading(false);
+
+    toast({
+      title: 'Success',
+      description: 'Sign out successful.',
+      status: 'success',
+      duration: 3000,
+    });
+
+    router.push('/');
+  };
+
+  useEffect(() => {
+    if (token) {
+      const payload: IJWTPayload = jwtDecode(token);
+
+      setUser({
+        name: payload.name,
+        email: payload.email,
+        role: payload.role,
+        organizer: payload.organizer,
+      });
+      setIsAuthenticated(true);
+    } else {
+      setUser(null);
+      setIsAuthenticated(false);
+    }
+  }, [token, isAuthenticated]);
+
+  useEffect(() => {
+    const token = getToken();
+
+    if (token) {
+      const payload: IJWTPayload = jwtDecode(token);
+
+      if (payload.exp * 1000 < Date.now()) {
+        removeToken();
+
+        toast({
+          title: 'Error',
+          description: 'Your session has expired.',
+          status: 'error',
+          duration: 3000,
+        });
+      }
+    }
+  }, [toast]);
+
+  return (
+    <AuthContext.Provider value={{ user, isLoading, signIn, signOut }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
