291075b40e266ebe42cdc9bb6eca672cc89e266d
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-10 11:05:56 +0700

    feat(pages): integrate SignInForm into sign in page

--- cms/src/pages/index.tsx
+++ cms/src/pages/index.tsx
@@ -1,9 +1,31 @@
+import Layout from 'components/layout';
+import { SignInForm } from 'components/organisms';
+
+import type { GetServerSideProps } from 'next/types';
+
 const Home = () => {
   return (
-    <div>
-      <h1>Hello, Matsuri CMS!</h1>
-    </div>
+    <Layout>
+      <SignInForm />
+    </Layout>
   );
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { token } = context.req.cookies;
+
+  if (token) {
+    return {
+      redirect: {
+        destination: '/dashboard',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
 };
 
 export default Home;
