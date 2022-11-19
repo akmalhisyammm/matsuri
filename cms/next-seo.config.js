d10486d3855bf70bc5c5f1cd74fab6903b8d5231
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-19 14:57:39 +0700

    chore: setup next-seo and next-sitemap configurations

---
+++ cms/next-seo.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-seo').DefaultSeoProps} */
+const defaultSEOConfig = {
+  title: undefined,
+  titleTemplate: '%s | Matsuri CMS',
+  defaultTitle: 'Matsuri CMS',
+  description: 'Online ticketing platform and event management system',
+  canonical: 'https://cms.matsuri.akmalhisyam.my.id',
+};
+
+module.exports = defaultSEOConfig;
---
+++ cms/next-sitemap.config.js
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://cms.matsuri.akmalhisyam.my.id',
+  generateRobotsTxt: true,
+};
