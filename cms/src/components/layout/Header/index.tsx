5b027d07e003dd75d82303c168a3048bfaca84c6
Muhammad Akmal Hisyam <muhammad.akmal@student.umn.ac.id>
2022-11-19 13:28:49 +0700

    feat(components): add user name and role in header layout

--- cms/src/components/layout/Header/index.tsx
+++ cms/src/components/layout/Header/index.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Box, Button, Flex, IconButton, useColorMode } from '@chakra-ui/react';
+import { Badge, Box, Button, HStack, IconButton, Text, useColorMode } from '@chakra-ui/react';
 import { RiMoonFill, RiSunLine } from 'react-icons/ri';
 
 import { AuthContext } from 'contexts/auth';
@@ -16,23 +16,41 @@ const Header = () => {
       paddingY={4}
       borderBottomWidth={1}
       backgroundColor={colorMode === 'light' ? 'gray.50' : 'gray.900'}>
-      <Flex gap={2} justifyContent="flex-end">
-        <Button
-          variant="outline"
-          colorScheme="red"
-          size="sm"
-          isLoading={authCtx.isLoading}
-          onClick={authCtx.signOut}>
-          Sign Out
-        </Button>
-        <IconButton
-          variant="ghost"
-          size="sm"
-          aria-label="Theme Toggle"
-          icon={colorMode === 'light' ? <RiMoonFill /> : <RiSunLine />}
-          onClick={toggleColorMode}
-        />
-      </Flex>
+      <HStack justifyContent="space-between">
+        <HStack>
+          <Badge
+            variant="outline"
+            colorScheme={
+              authCtx.user?.role === 'Owner'
+                ? 'purple'
+                : authCtx.user?.role === 'Organizer'
+                ? 'teal'
+                : 'cyan'
+            }>
+            {authCtx.user?.role}
+          </Badge>
+          <Text fontSize="sm" fontWeight={700}>
+            {authCtx.user?.name}
+          </Text>
+        </HStack>
+        <HStack>
+          <Button
+            variant="outline"
+            colorScheme="red"
+            size="sm"
+            isLoading={authCtx.isLoading}
+            onClick={authCtx.signOut}>
+            Sign Out
+          </Button>
+          <IconButton
+            variant="ghost"
+            size="sm"
+            aria-label="Theme Toggle"
+            icon={colorMode === 'light' ? <RiMoonFill /> : <RiSunLine />}
+            onClick={toggleColorMode}
+          />
+        </HStack>
+      </HStack>
     </Box>
   );
 };
